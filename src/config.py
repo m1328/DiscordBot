1e773aae7e271f5df660b0ee3d3740866826fea8
Maria Okuniewicz <okuniewicz13@gmail.com>
2025-05-07 19:53:04 +0200

    tests debug

--- src/config.py
+++ src/config.py
@@ -1,8 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
 COHERE_API_KEY = os.getenv("COHERE_API_KEY")
+
+print(f"TMDB_API_KEY = {TMDB_API_KEY}")
