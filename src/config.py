656b4621ec6d27518750c418cd9fd9f82bc46b55
Maria Okuniewicz <okuniewicz13@gmail.com>
2025-05-07 20:21:53 +0200

    tests debug

--- src/config.py
+++ src/config.py
@@ -1,9 +1,12 @@
 import os
-from pathlib import Path
-from dotenv import load_dotenv
 
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path, override=True)
+env_path = ".env"
+if os.path.exists(env_path):
+    with open(env_path, "r", encoding="utf-8", errors="ignore") as f:
+        for line in f:
+            if "=" in line:
+                key, value = line.strip().split("=", 1)
+                os.environ[key] = value
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 TMDB_API_KEY = os.getenv("TMDB_API_KEY")
